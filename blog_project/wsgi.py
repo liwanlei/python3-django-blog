708e3409ca7968171553a3653801a1aa8ae9a2df
lileilei <leileili126@163.com>
2017-11-05 13:01:56 +0800

    部署

--- blog_project/wsgi.py
+++ blog_project/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
+import sys
+import os
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_project.settings")
